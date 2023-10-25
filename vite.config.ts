04fc37abcd1c3cc31bf035224f0bd9a78d5677d3
Ciro Sira <ciroalejandro1@gmail.com>
2023-10-25 11:06:39 -0400

    add sass and normalize css

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/scss/_variables.scss';
+        
+        
+    `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
