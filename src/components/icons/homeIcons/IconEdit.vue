e4498900ea7c0a6476217da56fe83c545b5719d1
Ciro Sira <ciroalejandro1@gmail.com>
2023-10-26 00:37:16 -0400

    Icon home

---
+++ src/components/icons/homeIcons/IconEdit.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="10"
+    height="1448"
+    viewBox="0 0 10 1448"
+    fill="none"
+  >
+    <path
+      d="M10 5C10 2.23858 7.76142 0 5 0C2.23858 0 0 2.23858 0 5V1443C0 1445.76 2.23858 1448 5 1448C7.76142 1448 10 1445.76 10 1443V5Z"
+      fill="#D8D6DE"
+    />
+  </svg>
+</template>
