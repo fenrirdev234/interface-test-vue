32608f67e548b5171667c3a65fb1e383a468a062
Ciro Sira <ciroalejandro1@gmail.com>
2023-11-07 21:03:16 -0400

    VueOverlay

---
+++ src/lib/vueOverlayScrollbar/VueOverlayScollbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <OverlayScrollbarsComponent
+    :options="{
+      scrollbars: {
+        theme: 'os-theme-light'
+      }
+    }"
+    class="scroll__lib"
+  >
+    <slot></slot>
+  </OverlayScrollbarsComponent>
+</template>
+
+<script setup lang="ts">
+import 'overlayscrollbars/styles/overlayscrollbars.css'
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-vue'
+</script>
+
+<style scoped>
+.scroll__lib {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
