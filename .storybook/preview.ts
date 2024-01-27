bc6d628d767746609270bfd886ebd5c375ce3e55
Ciro Sira <ciroalejandro1@gmail.com>
2024-01-27 01:24:52 -0400

    add color, font and icons to doc storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from '@storybook/vue3'
+import '../src/assets/main.css'
+import '@fontsource-variable/montserrat'
+import '@fontsource/material-icons-round'
+import '@fontsource/material-icons'
 
 const preview: Preview = {
   parameters: {
