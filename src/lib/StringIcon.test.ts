c922bb1121e2fd38c0a8dd490f2ec3773d30e73e
Ciro Sira <ciroalejandro1@gmail.com>
2024-01-29 17:04:51 -0400

    other test case

--- src/lib/StringIcon.test.ts
+++ src/lib/StringIcon.test.ts
@@ -1,6 +1,13 @@
+// @ts-nocheck
 import { expect, test, it } from 'vitest'
 import { StringIcon } from './StringIcon'
 
 test('test type', () => {
   expect(StringIcon(4)).toBe(' ')
+})
+test('test sting random', () => {
+  expect(StringIcon('hi')).toBe(' ')
+})
+test('test correct string', () => {
+  expect(StringIcon('country')).toBe('location_on')
 })
